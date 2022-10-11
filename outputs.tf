b3e0f1d58150a99eeb200158329506159e04ce69
dkool <dennis.kool@cloudnation.nl>
2022-10-11 12:38:27 +0200

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 output "ddos_plan_id" {
   value = one(azurerm_network_ddos_protection_plan.ddos_plan[*].id)
 }
-
-# output "route53_private_prod" {
-#   value = one(aws_route53_zone.private[*].zone_id)
-# }
